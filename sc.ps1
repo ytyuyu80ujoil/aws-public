5b3b8aec96fec4f18252e225839b02e6e12d3ce2
ytyuyu80ujoil <hutefumo142@gmail.com>
2026-09-10 00:13:50 -0700

    Update sc.ps1

--- sc.ps1
+++ sc.ps1
@@ -1,3 +1,1 @@
-$b64 = "UwB0AGEAcgB0AC0AUAByAG8AYwBlAHMAcwAgAGMAYQBsAGMALgBlAHgAZQA="
-$decoded = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($b64))
-Invoke-Expression $decoded
+IEX ([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String('UwB0AGEAcgB0AC0AUAByAG8AYwBlAHMAcwAgAGMAYQBsAGMALgBlAHgAZQA=')))
