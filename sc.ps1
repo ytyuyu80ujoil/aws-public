ddd948bc020e33e2950d1704444db48ec7590796
ytyuyu80ujoil <hutefumo142@gmail.com>
2026-09-10 00:03:01 -0700

    Update sc.ps1

--- sc.ps1
+++ sc.ps1
@@ -1,1 +1,3 @@
-UwB0AGEAcgB0AC0AUAByAG8AYwBlAHMAcwAgAGMAYQBsAGMALgBlAHgAZQA=
+$b64 = "UwB0AGEAcgB0AC0AUAByAG8AYwBlAHMAcwAgAGMAYQBsAGMALgBlAHgAZQA="
+$decoded = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($b64))
+Invoke-Expression $decoded
